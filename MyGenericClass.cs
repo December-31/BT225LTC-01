cac0b25df6ac62bf8e5452f0deee7acccb557b19
Trương Văn Cường <vancuong31122k6@gmail.com>
2026-03-03 19:19:32 +0700

    Add files via upload

---
+++ MyGenericClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generic_CS
+{
+    public class MyGenericClass<T>
+    {
+        public void display()
+        {
+                Console.Write(typeof(T));
+        }
+    }
+}
